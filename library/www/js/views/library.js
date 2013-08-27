a2531eeb686ca2840087f200efbd4e7a5863111f
Patrick Fitzgerald <pfitzgerald@rubiconproject.com>
2013-08-26 17:01:05 -0700

    wire up adding books

--- library/www/js/views/library.js
+++ library/www/js/views/library.js
@@ -2,10 +2,17 @@ define(function (require) {
     'use strict';
     var LibraryCollection = require('collections/library');
     var BookView = require('views/book');
+    var BookModel = require('models/book');
     return Backbone.View.extend({
         el: '#books',
+        events: {
+            'click #add': 'addBook'
+        },
         initialize: function (initialBooks) {
             this.collection = new LibraryCollection(initialBooks);
+
+            this.listenTo(this.collection, 'add', this.renderBook);
+
             this.render();
         },
         render: function () {
@@ -18,6 +25,19 @@ define(function (require) {
                 model: item
             });
             this.$el.append(bookView.render().el);
+        },
+        addBook: function (event) {
+            event.preventDefault();
+            var formData = {};
+
+            $('#addBook div').children('input').each(function (item, el) {
+                var value = $(el).val();
+                if (value !== '') {
+                    formData[el.id] = value;
+                }
+            });
+
+            this.collection.add(new BookModel(formData));
         }
     });
 });
